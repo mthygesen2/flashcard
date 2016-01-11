92345621265617067a9154d96918cc8cf65577c3
Hillary Hintzen and Mao Thygesen <hillary07@gmail.com>
2016-01-11 15:45:48 -0800

    create jquery click function for list items

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,22 @@ $(document).ready(function(){
 
   $(".javascript").click(function() {
     $(".javascript p").toggle();
+    $(".javascript li").toggle();
+  });
+
+  $(".modulo").click(function() {
+    $(".modulo p").toggle();
+    $(".modulo li").toggle();
+  });
+
+  $(".operators").click(function() {
+    $(".operators p").toggle();
+    $(".operators li").toggle();
+  });
+
+  $(".Nan").click(function() {
+    $(".Nan p").toggle();
+    $(".Nan li").toggle();
   });
 
 
