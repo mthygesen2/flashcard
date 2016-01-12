3503111665c482c9d09910287977f984a567ff7b
Hillary Hintzen and Mao Thygesen <maothygesen@gmail.com>
2016-01-11 16:23:15 -0800

    create additional flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -20,5 +20,25 @@ $(document).ready(function(){
     $(".Nan li").toggle();
   });
 
+  $(".variable").click(function() {
+    $(".variable p").toggle();
+    $(".variable li").toggle();
+  });
+
+  $(".plus").click(function() {
+    $(".plus p").toggle();
+    $(".plus li").toggle();
+  });
+
+  $(".function").click(function() {
+    $(".function p").toggle();
+    $(".function li").toggle();
+  });
+
+  $(".string").click(function() {
+    $(".string p").toggle();
+    $(".string li").toggle();
+  });
+
 
 });
